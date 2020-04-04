736fae0ec9b75f43e40c211fb4cd21b5729febf2
nalam004 <47127568+nalam004@users.noreply.github.com>
2020-04-03 20:21:01 -0400

    Add files via upload

---
+++ profile.py
@@ -0,0 +1,24 @@
+from tkinter import *
+
+
+class ProfileWindow:
+
+    def __init__(self):
+        self.win = Tk()
+        # reset the window and background color
+        self.canvas = Canvas(self.win, width=600, height=500, bg='white')
+        self.canvas.pack(expand=YES, fill=BOTH)
+
+        # show window in center of the screen
+        width = self.win.winfo_screenwidth()
+        height = self.win.winfo_screenheight()
+        x = int(width / 2 - 600 / 2)
+        y = int(height / 2 - 500 / 2)
+        str1 = "600x500+" + str(x) + "+" + str(y)
+        self.win.geometry(str1)
+
+        # disable resize of the window
+        self.win.resizable(width=False, height=False)
+
+        # change the title of the window
+        self.win.title("Profile")
