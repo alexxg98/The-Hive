924a0d0a245de736b25b8a3a2d1ba838c483b14f
nalam004 <47127568+nalam004@users.noreply.github.com>
2020-04-13 21:57:45 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,6 +1,18 @@
 from tkinter import *
+from tkinter import messagebox
+
 import su
 import welcome
+import mysql.connector
+
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="alfheim",
+    database="TheHive"
+)
+
+cursor = db.cursor()
 
 
 class LoginWindow:
@@ -44,13 +56,28 @@ class LoginWindow:
         self.password = Entry(show='*', font='Courier 12')
         self.password.place(x=200, y=280)
 
-        self.button = Button(text="Login", font=('helvetica', 20), bg='dark green', fg='white', command=self.su)
+        self.button = Button(text="Login", font=('helvetica', 20), bg='dark green', fg='white', command=self.log_btn())
         self.button.place(x=170, y=310)
 
         self.button = Button(text="Back", font=('helvetica', 10), bg='dark green', fg='white', command=self.welcome)
         self.button.place(x=10, y=400)
 
         self.win.mainloop()
+
+    def log_btn(self):
+        username = self.username.get()
+        password = self.password.get()
+
+        cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s',
+                       (username, password))
+        account = cursor.fetchone()
+
+        if username == "" or password == "":
+            messagebox.showinfo("Login Status", "All Fields are Required")
+        elif account:
+            self.su()
+        else:
+            messagebox.showerror("Error", "Account does not exist!")
 
     def su(self):
         self.win.destroy()
