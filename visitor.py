5b46f105d7b9abcc2b8f28bbd4b284d79492650c
lwong0515 <43160578+lwong0515@users.noreply.github.com>
2020-05-08 16:10:26 -0400

    link to viewingpage

--- visitor.py
+++ visitor.py
@@ -1,5 +1,6 @@
 from tkinter import *
 import welcome
+import viewingpage
 
 
 class VisitorPage:
@@ -10,33 +11,42 @@ class VisitorPage:
         self.win.geometry('{}x{}'.format(800, 450))
         self.canvas = Canvas(self.win, bg='#454b54')
 
-        # self.pageTitle = Label(self.canvas, text="Visitor Page", bg="#454b54", fg="#f7cc35",
-        #                        font="Arial 20 bold")
-        self.welButton = Button(self.canvas, text="Login/Register", font='Arial 20 bold', bg='#454b54',
+        self.pageTitle = Label(self.canvas, text="Welcome to The Hive!", bg="#454b54", fg="#f7cc35",
+                               font="Arial 20 bold")
+        self.topProjectsLabel = Label(self.canvas, text="Top 3 Projects", bg="#454b54", fg="white",
+                               font="Arial 15 bold")
+        self.topUsersLabel = Label(self.canvas, text="Top 3 User Profiles", bg="#454b54", fg="white",
+                               font="Arial 15 bold")
+
+
+        self.welButton = Button(self.canvas, text="Login/Register", font='Arial 15 bold', bg='#454b54',
                                 fg="#f7cc35", command=self.welcome)
         # place holder for top 3 projects
         self.project1 = Button(self.canvas, text="Project 1", font='Arial 20 bold', bg='white',
-                                fg="black")
+                                fg="black", width = 10, height = 2)
         self.project2 = Button(self.canvas, text="Project 2", font='Arial 20 bold', bg='white',
-                                fg="black")
+                                fg="black", width = 10, height = 2)
         self.project3 = Button(self.canvas, text="Project 3", font='Arial 20 bold', bg='white',
-                                fg="black")
+                                fg="black", width = 10, height = 2)
         # place holder for top 3 users
         self.user1 = Button(self.canvas, text="User 1", font='Arial 20 bold', bg='white',
-                                fg="black")
+                                fg="black", width = 10, height = 2, command = self.viewingpage)
         self.user2 = Button(self.canvas, text="User 2", font='Arial 20 bold', bg='white',
-                                fg="black")
+                                fg="black", width = 10, height = 2)
         self.user3 = Button(self.canvas, text="User 3", font='Arial 20 bold', bg='white',
-                                fg="black")
+                                fg="black", width = 10, height = 2)
 
 
     def main(self):
         self.canvas.pack(expand=TRUE, fill=BOTH)
-        # self.pageTitle.pack(expand=TRUE)
-        self.welButton.place(x = 300, y = 50)
-        self.project1.place(x = 100, y = 225)
-        self.project2.place(x = 300, y = 225)
-        self.project3.place(x = 500, y = 225)
+        self.pageTitle.place(x = 50, y = 30)
+        self.topProjectsLabel.place(x = 50, y = 90)
+        self.topUsersLabel.place(x = 50, y = 285)
+
+        self.welButton.place(x = 600, y = 30)
+        self.project1.place(x = 100, y = 150)
+        self.project2.place(x = 300, y = 150)
+        self.project3.place(x = 500, y = 150)
 
         self.user1.place(x = 100, y = 325)
         self.user2.place(x = 300, y = 325)
@@ -48,6 +58,10 @@ class VisitorPage:
         self.win.destroy()
         wel = welcome.WelcomeWindow()
         wel.main()
+    def viewingpage(self):
+        self.win.destroy()
+        view = viewingpage.main()
+        view.main()
 
 
 if __name__ == "__main__":
