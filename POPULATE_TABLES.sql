9cb760899bfde5d4597d1093d04bdeb9311106c6
Alexandria Guo <44476083+alexxg98@users.noreply.github.com>
2020-05-10 11:59:54 -0400

    Create POPULATE_TABLES.sql

---
+++ POPULATE_TABLES.sql
@@ -0,0 +1,19 @@
+INSERT INTO 
+users(email, username, password, user_type) 
+VALUES 
+("niharika@gmail.com", "nalam001", "123", "SU"),
+("michael@gmail.com", "michael002", "123", "SU"),
+("alex@gmail.com", "alex003", "123", "SU"),
+("linda@gmail.com", "linda004", "123", "SU");
+
+INSERT INTO 
+users(email, username, password, reputation_score, user_type) 
+("fox@gmail.com", "mfox456", "123", 10, "OU");   
+
+INSERT INTO 
+pending_users(name, email, reference, interest, credential) 
+VALUES 
+("Nicky Minaj", "nicki@gmail.com", "reference", "interest", "credential"),
+("Aster Fallon", "aster@gmail.com", "reference", "interest", "credential"),
+("Adele", "adele@gmail.com", "reference", "interest", "credential"),
+("Romeo", "romeo@gmail.com", "reference", "interest", "credential");
