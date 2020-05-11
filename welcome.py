223f2144848bd90bc11869ceaf83a4862ccdc07a
nalam004 <47127568+nalam004@users.noreply.github.com>
2020-05-11 16:27:45 -0400

    Add files via upload

--- welcome.py
+++ welcome.py
@@ -9,25 +9,24 @@ class WelcomeWindow:
         self.win = Tk()
         self.win.title("The Hive")
         self.win.geometry('{}x{}'.format(800, 450))
-        self.canvas = Canvas(self.win, bg='#454b54')
-
+        self.canvas = Canvas(self.win, bg='#36393F')
+        self.frame = Frame(self.canvas, bg='#36393F')
         self.img = PhotoImage(file='images/icon.png')
-        self.icon = Label(self.canvas, image=self.img, bg='#454b54')
+        self.icon = Label(self.frame, image=self.img, bg='#36393F')
 
-        self.pageTitle = Label(self.canvas, text="Welcome to The Hive", bg='#454b54',
-                               font="Arial 20 bold", fg='#f7cc35')
-
-        self.logButton = Button(self.canvas, text="Login", font='Arial 20 bold',
+        self.logButton = Button(self.frame, text="Login", font='Arial 20 bold',
                                 bg='#454b54', fg='#f7cc35', command=self.login)
-        self.regButton = Button(self.canvas, text="Register", font='Arial 20 bold',
+        self.regButton = Button(self.frame, text="Register", font='Arial 20 bold',
                                 bg='#454b54', fg='#f7cc35', command=self.register)
 
     def main(self):
         self.canvas.pack(expand=TRUE, fill=BOTH)
-        self.icon.pack(expand=TRUE)
-        self.pageTitle.pack(expand=TRUE)
-        self.logButton.pack(expand=TRUE)
-        self.regButton.pack(expand=TRUE)
+        self.frame.pack(expand=TRUE)
+        self.logButton.grid(row=0, column=0)
+        self.icon.grid(row=1, column=1)
+        self.regButton.grid(row=0, column=2)
+        Label(self.frame, text="The Hive", bg='#36393F',
+              font="Arial 20 bold", fg='#f7cc35').grid(row=2, column=1)
         self.win.mainloop()
 
     def login(self):
