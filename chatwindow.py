111724c7e9bd718d340dc7ea921c8606d29d6955
lwong0515 <43160578+lwong0515@users.noreply.github.com>
2020-05-11 19:42:56 -0400

    save chat history

--- chatwindow.py
+++ chatwindow.py
@@ -1,5 +1,6 @@
 from tkinter import*
 import mysql.connector
+import sys
 import db
 
 #Get and store user info from database
@@ -22,25 +23,35 @@ def Input_Enter(event):
     messages.configure(state='disabled')
     user_input.set('')
     return "break"
+
+def savelog():
+    chatMessages = messages.get("1.0", END)
+    file = open('chat_log.txt', 'w')
+    file.write(chatMessages)
+
 window = Tk()
 
 chatBox = Scrollbar(window)
-messages = Text(window, wrap='word', state='disabled', yscrollcommand=chatBox.set,bg = "#36393F", fg = "white")
+messages = Text(window, wrap='word', state='disabled', yscrollcommand=chatBox.set)
 chatBox.configure(command=messages.yview)
-messages.grid(row = 0, column = 0, columnspan = 2)
+messages.grid(row = 0, column = 0, columnspan = 3)
 
 user_input = StringVar()
-input_field = Entry(window, width = 100, text = user_input)
+input_field = Entry(window, width = 100, text = user_input, bg="#e6f2ff")
 input_field.grid(row = 1, column = 0)
-# place holder for username
-# username = "[Username]: "
-
 
 frame = Frame(window)
 input_field.bind("<Return>", Input_Enter)
 frame.grid(row = 0, column = 0)
 send = Button(window, text = "Send", command = Press_Button, bg = "black", fg = "white")
-send.grid(row = 1, column = 1, columnspan = 2)
+send.grid(row = 1, column = 1)
+save = Button(window, text = "Save Log", bg = "red", fg = "white", command = lambda: savelog())
+save.grid(row = 1, column = 2)
+
+messages.configure(state='normal')
+with open('chat_log.txt', 'r') as f:
+    messages.insert(INSERT, f.read())
+messages.configure(state='normal')
 
 window.resizable(False, False)
 window.title("Hive Chat")
