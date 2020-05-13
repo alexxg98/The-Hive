e65bab4cc1e6d1c22c9ffa3fdadf5cac965af7e3
lwong0515 <43160578+lwong0515@users.noreply.github.com>
2020-05-12 21:47:23 -0400

    fix vote update

--- voting.py
+++ voting.py
@@ -1,7 +1,6 @@
 import os
 from tkinter import *
 from tkinter.ttk import Combobox
-from prettytable import PrettyTable
 import db
 
 def inputUsers():
@@ -15,16 +14,13 @@ class VotingPage:
     def __init__(self):
         self.win = Tk()
         self.win.title("Voting ")
-        self.win.geometry('{}x{}'.format(800, 450))
+        self.win.geometry('{}x{}'.format(600, 450))
         self.canvas = Canvas(self.win, bg='#454b54')
-        self.frame = Frame(self.canvas, bg='#454b54', width=600, height=340)
-
-        headerFormat = ["Your Name","Reason","SU Candidate"]
-        self.table = PrettyTable(headerFormat)
+        self.frame = Frame(self.canvas, bg='#454b54', width=300, height=340)
 
         self.VIPlist = StringVar()
 
-        self.votepage = Text(self.frame, width=80, height=15)
+        self.votepage = Text(self.frame, width=50, height=15)
 
         self.selectVIP = Combobox(self.frame, width=15, textvariable=self.VIPlist, state="readonly")
         self.selectVIP['values'] = inputUsers()
@@ -38,12 +34,12 @@ class VotingPage:
         Label(self.frame, text="Select a VIP to be promoted to SU", bg="#454b54", fg="#f7cc35",
               font="Arial 10 bold").grid(row=0, column=0, columnspan=4)
 
-        self.selectVIP.grid(row=1, column=2)
+        self.selectVIP.grid(row=1, column=1)
         self.selectVIP.current(0)
 
         self.votepage.grid(row=2, columnspan=4, pady=10)
         self.vote.grid(row=3, column=0)
-        self.view.grid(row=3, column=2)
+        self.view.grid(row=3, column=3)
 
         self.win.mainloop()
 
@@ -55,19 +51,19 @@ class VotingPage:
         db.cursor.execute("UPDATE users SET votes = %s WHERE username = %s",(incVote, vipChoice))
 
     def printVotes(self):
-        self.table.clear_rows()
-
+        self.votepage.delete(1.0, END)
         db.cursor.execute("SELECT username FROM users WHERE user_type = 'VIP'")
         data = []
         for row in db.cursor.fetchall():
             data.append(row[0])
 
         for user in data:
+
             db.cursor.execute("SELECT votes FROM users WHERE username = '%s'"%user)
             numVote = db.cursor.fetchone()[0]
             display = user + ": " + str(numVote) + "\n\n"
             self.votepage.insert(INSERT,display)
-            self.votepage.see(INSERT)
+            # self.votepage.see(INSERT)
 
 if __name__ == "__main__":
     x = VotingPage()
