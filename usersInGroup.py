0a64608b3e995d27e37252bf760df27ca39135af
lwong0515 <43160578+lwong0515@users.noreply.github.com>
2020-05-13 19:22:50 -0400

    add button

--- usersInGroup.py
+++ usersInGroup.py
@@ -16,6 +16,8 @@ class SuperUser:
         self.widget = Label(self.canvas, text='Users in Group: ', font='Arial 15 bold',fg='white', bg='#454b54')
         self.close_btn = Button(self.canvas, text="Close", font='Arial 15 bold', bg='#454b54',
                                    fg="#f7cc35", command=quit)
+        self.add_btn = Button(self.canvas, text = "Add User to Group", font='Arial 15 bold', bg='#454b54',
+                                   fg="#f7cc35")
         self.list = Treeview(self.canvas, columns=(1, 2, 3, 4, 5), show="headings", height="15")
 
     def main(self):
@@ -31,15 +33,15 @@ class SuperUser:
         self.list.heading(4, text="Type")
         self.list.column(4, width=50)
 
-        db.cursor.execute("SELECT id FROM projects WHERE viewing = 'ON'")
-        groupID = db.cursor.fetchone()[0]
+        groupID = db.getGroupID()
         db.cursor.execute("SELECT A.username,email,reputation_score,user_type FROM users A\
                      JOIN group_membership B on A.username = B.username where B.group_id =%s",(groupID,))
 
         for row in db.cursor.fetchall():
             self.list.insert('', END, values=row)
 
-        self.close_btn.pack(expand=TRUE)
+        self.close_btn.place(x = 300, y = 354)
+        self.add_btn.place(x = 380, y = 354)
         self.win.mainloop()
 
 if __name__ == "__main__":
