19d6eb5466dc40084cf330b505152c80791d3af7
lwong0515 <43160578+lwong0515@users.noreply.github.com>
2020-05-13 16:46:07 -0400

    Update schedule.py

--- schedule.py
+++ schedule.py
@@ -21,7 +21,7 @@ class SchedulePage:
         self.startTime = StringVar()
         self.endTime = StringVar()
 
-        self.schedule = Text(self.frame, width=50, height=15)
+        self.schedule = Text(self.frame, width=80, height=15)
 
         self.TimeStartDrop = Combobox(self.frame, width=10, textvariable=self.startTime, state="readonly")
         self.TimeStartDrop['values'] = ("-Start-", "8:00am", "9:00am", "10:00am", "11:00am", "12:00pm", "1:00pm",
