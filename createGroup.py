9a16f9ece7bcc4398132d0abf9259e232e8cf0c0
Alexandria <lexieguo@gmail.com>
2020-05-13 18:27:44 -0400

    Make initial post in new group. Postdoc works in new group now

--- createGroup.py
+++ createGroup.py
@@ -39,7 +39,7 @@ class CreateGroup:
             self.tree.insert('', END, values=row)
 
         self.win.mainloop()
-        
+
     def invite(self):
         try:
             for selected_item in self.tree.selection():
@@ -57,7 +57,15 @@ class CreateGroup:
 
     def create_group(self):
         description = self.textBox.get("1.0", "end-1c")
-        db.cursor.execute("INSERT INTO projects(name, description) VALUES(%s, %s)", (self.name.get(), description))
+        initialPost = "Hello. Welcome to the group!"
+
+        db.cursor.execute("INSERT INTO projects(name, description, creator) VALUES(%s, %s, %s)", (self.name.get(), description, db.getName()))
+
+        db.cursor.execute("SELECT id FROM projects WHERE name = '%s'" %self.name.get())
+        groupID = db.cursor.fetchone()[0]
+        print(groupID)
+
+        db.cursor.execute("INSERT into posts(postid, group_id, username, content) VALUES (%s, %s, %s, %s)",(1, groupID, db.getName(), initialPost))
 
 
 if __name__ == "__main__":
